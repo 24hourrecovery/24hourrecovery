1ba1eeaec381b7c2a3b6c82187b22934371852f8
24hourrecovery <smartrecoveryian@gmail.com>
2024-08-25 06:04:14 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -558,7 +558,6 @@ const meetings = [
   {audience:["Adults Welcome","Women Only"],dataSource:"uk",duration:90,facilitator:[598],id:1404,language:"English",location:"National",program:"SMART Recovery",schedule:{day:4,time:"14:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"uk",duration:120,facilitator:[599],id:1460,language:"Polish",location:"Edinburgh, Scotland",program:"SMART Recovery",schedule:{day:3,time:"18:00Z"},type:"Online"},
   {audience:["Adults Welcome","Women Only"],canceled:"2024-08-26T11:00:00Z",dataSource:"uk",duration:90,facilitator:[600],id:1467,language:"English",location:"National",program:"SMART Veterans",schedule:{day:1,time:"11:00Z"},type:"Online"},
-  {audience:["Adults Welcome"],dataSource:"uk",duration:90,facilitator:[601],id:1478,language:"English",location:"National",program:"SMART Recovery",schedule:{day:0,time:"10:00Z"},type:"Online"},
   {audience:["Adults Welcome","Veterans"],dataSource:"uk",duration:90,facilitator:[600],id:1535,language:"English",location:"National",program:"SMART Veterans",schedule:{day:1,time:"18:00Z"},type:"Online"},
   {audience:["Adults Welcome","Veterans"],dataSource:"uk",duration:90,facilitator:[600],id:1542,language:"English",location:"National",program:"SMART Veterans",schedule:{day:3,time:"18:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"uk",duration:90,facilitator:[602],id:1547,language:"English",location:"Wallsend, England",program:"SMART Recovery",schedule:{day:3,time:"09:30Z"},type:"Online"}
@@ -1175,4 +1174,4 @@ const facilitators = {
   615:["Spencer","Arizona"],
   616:["Andy"]
 };
-const cachedAt = "2024-08-25T10:03:04Z";
+const cachedAt = "2024-08-25T11:03:04Z";
