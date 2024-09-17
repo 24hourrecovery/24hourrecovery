41a80485cc56b43e02286cc80a9685044c55e057
24hourrecovery <smartrecoveryian@gmail.com>
2024-09-17 11:04:11 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -416,6 +416,7 @@ const meetings = [
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[481,482],id:7270,language:"English",location:"New York, New York",program:"4-Point Recovery",schedule:{day:4,time:"16:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:90,facilitator:[483],id:7274,language:"English",location:"New York",program:"4-Point Recovery",schedule:{day:0,time:"22:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[484,485],id:7275,language:"English",location:"Swansea, Illinois",program:"4-Point Recovery",schedule:{day:5,time:"16:00Z"},type:"Online"},
+  {audience:["Family & Friends Only"],dataSource:"na",duration:60,facilitator:[608,609],id:7291,language:"English",location:"Syracuse, New York",program:"Family & Friends",schedule:{day:2,time:"16:00Z",week:[-1]},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[486],id:7301,language:"English",location:"Toronto, Ontario",program:"4-Point Recovery",schedule:{day:1,time:"16:00Z"},type:"Online"},
   {audience:["Family & Friends Only"],dataSource:"na",duration:90,facilitator:[469,487],id:7302,language:"English",location:"National",program:"Family & Friends",schedule:{day:6,time:"15:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:90,facilitator:[488],id:7312,language:"English",location:"Boone, North Carolina",program:"4-Point Recovery",schedule:{day:4,time:"21:30Z"},type:"Both"},
@@ -1160,6 +1161,8 @@ const facilitators = {
   605:["Eboni Jewel Sears","Nevada"],
   606:["Andrew","Ontario"],
   607:["Diana"],
+  608:["Kaelyn Dowd"],
+  609:["Bethani Rahrle White"],
   610:["Melissa"],
   612:["Sharm","National"],
   613:["Gordon M"],
@@ -1189,4 +1192,4 @@ const facilitators = {
   651:["Cheryl P."],
   652:["Jodi S.","Ohio"]
 };
-const cachedAt = "2024-09-17T15:03:04Z";
+const cachedAt = "2024-09-17T16:03:04Z";
