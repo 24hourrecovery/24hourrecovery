daf16dbe742f17545733cc3163ea01f73c67e932
24hourrecovery <smartrecoveryian@gmail.com>
2024-09-19 02:04:05 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -28,6 +28,7 @@ const meetings = [
   {audience:["Drug and Alcohol Focus","Youth only"],dataSource:"au",duration:90,facilitator:[26],id:1107,language:"English",location:"National",program:"Youth",schedule:{day:3,time:"09:00Z"},type:"Online"},
   {audience:["LGBTIQ+ only"],dataSource:"au",duration:90,facilitator:[28,29,30],id:1126,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"08:00Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[31],id:1129,language:"English",location:"Burnie, Tasmania",program:"Standard",schedule:{day:2,time:"08:30Z"},type:"Online"},
+  {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[13],id:1151,language:"English",location:"National",program:"Standard",schedule:{day:4,time:"04:00Z",week:[2,4]},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[32],id:1182,language:"English",location:"National",program:"Standard",schedule:{day:3,time:"04:30Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[33],id:1192,language:"English",location:"National",program:"Standard",schedule:{day:1,time:"04:30Z"},type:"Online"},
   {audience:["Youth only"],dataSource:"au",duration:90,facilitator:[34],id:1209,language:"English",location:"National",program:"Youth",schedule:{day:3,time:"01:00Z"},type:"Online"},
@@ -579,6 +580,7 @@ const facilitators = {
   10:["Kristalleni"],
   11:["Meridian"],
   12:["Nigel"],
+  13:["Smart Recovery Footscray"],
   14:["Odyssey House CES"],
   15:["Smart Perth - Palmerston"],
   16:["Stuart"],
@@ -1180,4 +1182,4 @@ const facilitators = {
   652:["Jodi S.","Ohio"],
   653:["Mara"]
 };
-const cachedAt = "2024-09-19T06:03:04Z";
+const cachedAt = "2024-09-19T07:03:03Z";
