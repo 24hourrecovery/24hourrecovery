278b8817221cd60663af54c6fc197ebf5b0638f5
24hourrecovery <smartrecoveryian@gmail.com>
2024-09-25 10:09:44 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -1183,4 +1183,4 @@ const facilitators = {
   664:["Tyler Hutchison","Michigan"],
   665:["Curtis J","Oregon"]
 };
-const cachedAt = "2024-09-25T14:03:05Z";
+const cachedAt = "2024-09-25T15:03:04Z";
