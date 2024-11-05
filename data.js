72fb8fe944e9553fb1e7f36361de63fd608ac027
24hourrecovery <smartrecoveryian@gmail.com>
2024-11-04 23:04:18 -0600

    Automated commit: update data

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ const meetings = [
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[15,16],id:1043,language:"English",location:"Perth, Western Australia",program:"Standard",schedule:{day:5,time:"06:00Z"},type:"Online"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[671],id:1045,language:"English",location:"National",program:"Standard",schedule:{day:0,time:"06:00Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:60,facilitator:[24],id:1065,language:"English",location:"Bronte, New South Wales",program:"Standard",schedule:{day:1,time:"02:00Z"},type:"Both"},
-  {audience:["All Welcome"],canceled:"2024-11-05T04:00:00Z",dataSource:"au",duration:90,facilitator:[25],id:1071,language:"English",location:"Frankston, Victoria",program:"Standard",schedule:{day:2,time:"04:00Z"},type:"Both"},
+  {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[25],id:1071,language:"English",location:"Frankston, Victoria",program:"Standard",schedule:{day:2,time:"04:00Z"},type:"Both"},
   {audience:["All Welcome","Youth only"],dataSource:"au",duration:90,facilitator:[26],id:1106,language:"Chinese",location:"National",program:"Youth",schedule:{day:3,time:"05:30Z"},type:"Online"},
   {audience:["Drug and Alcohol Focus","Youth only"],dataSource:"au",duration:90,facilitator:[26],id:1107,language:"English",location:"National",program:"Youth",schedule:{day:3,time:"08:00Z"},type:"Online"},
   {audience:["LGBTIQ+ only"],dataSource:"au",duration:90,facilitator:[28,29,30],id:1126,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"07:00Z"},type:"Online"},
@@ -1126,7 +1126,6 @@ const facilitators = {
   613:["Gordon M"],
   614:["Nate"],
   618:["Mike Christner"],
-  619:["James","National"],
   620:["Thane"],
   621:["Debbie"],
   622:["Dee"],
@@ -1209,4 +1208,4 @@ const facilitators = {
   715:["Waldemar"],
   716:["Michelle","Georgia"]
 };
-const cachedAt = "2024-11-05T04:03:04Z";
+const cachedAt = "2024-11-05T05:03:03Z";
