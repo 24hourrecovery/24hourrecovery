052768bbf140fc56c08654a8c45094d098381e59
24hourrecovery <smartrecoveryian@gmail.com>
2024-11-06 01:09:35 -0600

    Automated commit: update data

--- data.js
+++ data.js
@@ -22,7 +22,7 @@ const meetings = [
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[671],id:1045,language:"English",location:"National",program:"Standard",schedule:{day:0,time:"06:00Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:60,facilitator:[24],id:1065,language:"English",location:"Bronte, New South Wales",program:"Standard",schedule:{day:1,time:"02:00Z"},type:"Both"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[25],id:1071,language:"English",location:"Frankston, Victoria",program:"Standard",schedule:{day:2,time:"04:00Z"},type:"Both"},
-  {audience:["All Welcome","Youth only"],canceled:"2024-11-06T05:30:00Z",dataSource:"au",duration:90,facilitator:[26],id:1106,language:"Chinese",location:"National",program:"Youth",schedule:{day:3,time:"05:30Z"},type:"Online"},
+  {audience:["All Welcome","Youth only"],dataSource:"au",duration:90,facilitator:[26],id:1106,language:"Chinese",location:"National",program:"Youth",schedule:{day:3,time:"05:30Z"},type:"Online"},
   {audience:["Drug and Alcohol Focus","Youth only"],dataSource:"au",duration:90,facilitator:[26],id:1107,language:"English",location:"National",program:"Youth",schedule:{day:3,time:"08:00Z"},type:"Online"},
   {audience:["LGBTIQ+ only"],dataSource:"au",duration:90,facilitator:[28,29,30],id:1126,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"07:00Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[31],id:1129,language:"English",location:"Burnie, Tasmania",program:"Standard",schedule:{day:2,time:"07:30Z"},type:"Online"},
@@ -604,7 +604,6 @@ const facilitators = {
   19:["Abbie"],
   20:["Elizabeth"],
   21:["Simon","Thursday"],
-  23:["Mark"],
   24:["Dave","New South Wales"],
   25:["Dan"],
   26:["Ivan"],
@@ -1208,4 +1207,4 @@ const facilitators = {
   719:["Stew Talbot"],
   720:["Rachel Boyle"]
 };
-const cachedAt = "2024-11-06T06:03:04Z";
+const cachedAt = "2024-11-06T07:03:04Z";
