c656ec838d1063e627bd59626a308bd7bce1569e
24hourrecovery <smartrecoveryian@gmail.com>
2024-12-13 08:09:19 -0600

    Automated commit: update data

--- data.js
+++ data.js
@@ -567,7 +567,8 @@ const meetings = [
   {audience:["Adults Welcome","Family & Friends Only"],dataSource:"uk",duration:90,facilitator:[575],id:1567,language:"English",location:"National",program:"Veterans Family and Friends",schedule:{day:4,time:"12:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"uk",duration:90,facilitator:[677],id:1600,language:"English",location:"National",program:"SMART Recovery",schedule:{day:3,time:"19:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"uk",duration:90,facilitator:[678],id:1601,language:"English",location:"National",program:"SMART Recovery",schedule:{day:6,time:"11:00Z"},type:"Online"},
-  {audience:["Adults Welcome","Military Veterans"],dataSource:"uk",duration:60,facilitator:[701],id:1603,language:"English",location:"Stevenage, England",program:"SMART Veterans",schedule:{day:2,time:"18:30Z"},type:"Online"}
+  {audience:["Adults Welcome","Military Veterans"],dataSource:"uk",duration:60,facilitator:[701],id:1603,language:"English",location:"Stevenage, England",program:"SMART Veterans",schedule:{day:2,time:"18:30Z"},type:"Online"},
+  {audience:["Adults Welcome"],dataSource:"uk",duration:90,facilitator:[752,753],id:1640,language:"Punjabi",location:"National",program:"SMART Recovery",schedule:{day:0,time:"08:00Z"},type:"Online"}
 ];
 const facilitators = {
   1:["Joel"],
@@ -1182,6 +1183,8 @@ const facilitators = {
   748:["Tim van den Berg"],
   749:["Shelbie"],
   750:["David S."],
-  751:["Brettney T."]
+  751:["Brettney T."],
+  752:["Shiv Sharma"],
+  753:["Dr. Bhagat Ram"]
 };
-const cachedAt = "2024-12-13T13:03:04Z";
+const cachedAt = "2024-12-13T14:03:05Z";
