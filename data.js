d335c0ac7c8f7c65c3d2f541bdcf3d13dc0af7fe
24hourrecovery <smartrecoveryian@gmail.com>
2025-01-28 01:10:39 -0600

    Automated commit: update data

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const meetings = [
   {audience:["Men only"],dataSource:"au",duration:90,facilitator:[604,689],id:1006,language:"English",location:"National",program:"Standard",schedule:{day:6,time:"08:00Z"},type:"Online"},
   {audience:["Youth only"],dataSource:"au",duration:90,facilitator:[1,2],id:1007,language:"English",location:"National",program:"Standard",schedule:{day:3,time:"04:30Z"},type:"Online"},
-  {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[5],id:1011,language:"English",location:"National",program:"Standard",schedule:{day:1,time:"06:30Z"},type:"Online"},
+  {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[799],id:1011,language:"English",location:"National",program:"Standard",schedule:{day:1,time:"06:30Z"},type:"Online"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[6],id:1016,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"01:00Z"},type:"Online"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[7],id:1020,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"03:00Z"},type:"Online"},
   {audience:["Pilots only"],dataSource:"au",duration:90,facilitator:[8],id:1022,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"07:00Z"},type:"Online"},
@@ -582,7 +582,6 @@ const meetings = [
 const facilitators = {
   1:["Joel"],
   2:["Alice"],
-  5:["Nick","National"],
   6:["Isabella"],
   7:["Watershed Lives Lived Well"],
   8:["Andrew","National"],
@@ -1195,6 +1194,7 @@ const facilitators = {
   795:["Chris","Alberta"],
   796:["Helena Machado"],
   797:["Antonio"],
-  798:["Bev"]
+  798:["Bev"],
+  799:["Nick","National"]
 };
-const cachedAt = "2025-01-28T06:03:04Z";
+const cachedAt = "2025-01-28T07:03:04Z";
