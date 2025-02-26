0e1fed63bb3b1f2961ee3d7314c3148fc2e76cb5
24hourrecovery <smartrecoveryian@gmail.com>
2025-02-26 02:04:26 -0600

    Automated commit: update data

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const meetings = [
   {audience:["Men only"],dataSource:"au",duration:90,facilitator:[604,689],id:1006,language:"English",location:"National",program:"Standard",schedule:{day:6,time:"08:00Z"},type:"Online"},
+  {audience:["Youth only"],dataSource:"au",duration:90,facilitator:[1,2],id:1007,language:"English",location:"National",program:"Standard",schedule:{day:3,time:"04:30Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[799],id:1011,language:"English",location:"National",program:"Standard",schedule:{day:1,time:"06:30Z"},type:"Online"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[6],id:1016,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"01:00Z"},type:"Online"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[7],id:1020,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"03:00Z"},type:"Online"},
@@ -1261,4 +1262,4 @@ const facilitators = {
   865:["Rey"],
   866:["Sandra"]
 };
-const cachedAt = "2025-02-26T07:03:04Z";
+const cachedAt = "2025-02-26T08:03:04Z";
