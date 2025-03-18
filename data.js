b065db68276544a49ed6da579e0ef50005694349
24hourrecovery <smartrecoveryian@gmail.com>
2025-03-17 21:12:52 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -289,7 +289,7 @@ const meetings = [
   {audience:["Adults Welcome"],dataSource:"na",duration:90,facilitator:[140,387],id:6380,language:"English",location:"Troy, Michigan",program:"4-Point Recovery",schedule:{day:2,time:"13:30Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:90,facilitator:[140,387],id:6381,language:"English",location:"Troy, Michigan",program:"4-Point Recovery",schedule:{day:4,time:"23:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:90,facilitator:[101,102,103],id:6383,language:"English",location:"Overland Park, Kansas",program:"4-Point Recovery",schedule:{day:2,time:"00:00Z"},type:"Online"},
-  {audience:["LGBTQIA+"],dataSource:"na",duration:60,facilitator:[388],id:6403,language:"English",location:"St. Louis, Missouri",program:"4-Point Recovery",schedule:{day:6,time:"15:00Z"},type:"Online"},
+  {audience:["LGBTQIA+"],dataSource:"na",duration:60,facilitator:[908],id:6403,language:"English",location:"St. Louis, Missouri",program:"4-Point Recovery",schedule:{day:6,time:"15:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:90,facilitator:[69,70,648],id:641,language:"English",location:"Concord, Massachusetts",program:"4-Point Recovery",schedule:{day:1,time:"23:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[389],id:6414,language:"English",location:"Culpeper, Virginia",program:"4-Point Recovery",schedule:{day:2,time:"22:30Z"},type:"Both"},
   {audience:["Men"],dataSource:"na",duration:60,facilitator:[322],id:6423,language:"English",location:"Schaumburg, Illinois",program:"4-Point Recovery",schedule:{day:4,time:"23:00Z"},type:"Online"},
@@ -924,7 +924,6 @@ const facilitators = {
   385:["Sina Martel"],
   386:["Catherine"],
   387:["Steve Norris"],
-  388:["Cas Fraser"],
   389:["Gaelen E."],
   390:["Michelle","Rhode Island"],
   392:["Carolyn"],
@@ -1270,6 +1269,7 @@ const facilitators = {
   901:["Thomas A"],
   902:["Josephine McCormick"],
   906:["Sarah M-I"],
-  907:["Brian D."]
+  907:["Brian D."],
+  908:["Scout Fraser"]
 };
-const cachedAt = "2025-03-18T01:03:04Z";
+const cachedAt = "2025-03-18T02:03:04Z";
