24ec8d4270885f3335e54cb8d623c40db4a04ac3
24hourrecovery <smartrecoveryian@gmail.com>
2025-03-31 04:04:31 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 const meetings = [
-  {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[799],id:1011,language:"English",location:"National",program:"Standard",schedule:{day:1,time:"06:30Z"},type:"Online"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[6],id:1016,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"01:00Z"},type:"Online"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[7],id:1020,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"03:00Z"},type:"Online"},
   {audience:["Pilots only"],dataSource:"au",duration:90,facilitator:[8],id:1022,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"07:00Z"},type:"Online"},
@@ -1258,4 +1257,4 @@ const facilitators = {
   934:["Tom V."],
   935:["Sooz D"]
 };
-const cachedAt = "2025-03-31T08:03:04Z";
+const cachedAt = "2025-03-31T09:03:04Z";
