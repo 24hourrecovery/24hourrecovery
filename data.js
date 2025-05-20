e116628338c8b41924f3867f9fcd3c830dd3bc17
24hourrecovery <smartrecoveryian@gmail.com>
2025-05-20 05:11:50 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 const meetings = [
-  {audience:["Pilots only"],dataSource:"au",duration:90,facilitator:[8],id:1022,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"08:00Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:60,facilitator:[24],id:1065,language:"English",location:"Bronte, New South Wales",program:"Standard",schedule:{day:1,time:"03:00Z"},type:"Both"},
   {audience:["Female only"],dataSource:"au",duration:90,facilitator:[688],id:1333,language:"English",location:"Edinburgh North, South Australia",program:"Standard",schedule:{day:4,time:"02:30Z",week:[2,4]},type:"Both"},
   {audience:["Sex Worker"],dataSource:"au",duration:90,facilitator:[947,948],id:1405,language:"English",location:"National",program:"Standard",schedule:{day:3,time:"05:00Z",week:[2,3,4]},type:"Both"},
@@ -1203,4 +1202,4 @@ const facilitators = {
   1050:["Kris Runske"],
   1051:["Kaitlyn M"]
 };
-const cachedAt = "2025-05-20T09:03:04Z";
+const cachedAt = "2025-05-20T10:03:04Z";
