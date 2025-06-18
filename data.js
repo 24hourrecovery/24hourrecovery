d4763e6855069040208eeb3aa6867e1ac6bb3088
24hourrecovery <smartrecoveryian@gmail.com>
2025-06-18 00:14:17 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -1183,4 +1183,4 @@ const facilitators = {
   1172:["South African Meeting Host Team"],
   1179:["Jack McGraw"]
 };
-const cachedAt = "2025-06-18T04:03:04Z";
+const cachedAt = "2025-06-18T05:03:04Z";
