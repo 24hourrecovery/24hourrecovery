e98d8af5a785c170efc2ea45516bf949b4baad35
24hourrecovery <smartrecoveryian@gmail.com>
2025-07-19 15:04:49 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -1035,12 +1035,13 @@ const facilitators = {
   826:["SMART Espanol Women&#x27;s Meetings"],
   827:["Oscar"],
   828:["Nadezhda Borzunova"],
+  829:["Tomasz Nawrocki"],
   830:["Kurt Brewster"],
   831:["Gerardo Matamoros","Global / United States"],
   832:["Juan Negron"],
   834:["Fernando Cote"],
   835:["Lili"],
-  836:["Jesus Cisneros","Global / United States"],
+  836:["Jesus Cisneros","United States / Global"],
   837:["Anabel"],
   838:["Vitaly Kozyrev"],
   839:["Dimitrii Frolov"],
@@ -1132,6 +1133,7 @@ const facilitators = {
   1011:["Jonathon Maxgay"],
   1013:["Paul","Colorado"],
   1021:["Leonard Murphy"],
+  1050:["Kris Runske"],
   1055:["David Wilkinson"],
   1059:["Samantha T."],
   1062:["Vikki P"],
@@ -1167,7 +1169,6 @@ const facilitators = {
   1230:["Greg H"],
   1231:["Frank","New Jersey"],
   1232:["Angela Bekederemo"],
-  1239:["Ifechidere Ugwuibe"],
   1245:["Jessica B"],
   1246:["Clea P"],
   1251:["Justin"],
@@ -1188,4 +1189,4 @@ const facilitators = {
   1297:["Myranda Reyes"],
   1300:["Whit D."]
 };
-const cachedAt = "2025-07-19T19:03:05Z";
+const cachedAt = "2025-07-19T20:03:04Z";
