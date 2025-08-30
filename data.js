15fdc295998a8861c424310536282a9f78a3e75c
24hourrecovery <smartrecoveryian@gmail.com>
2025-08-30 04:05:00 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -1017,7 +1017,7 @@ const facilitators = {
   833:["Javier"],
   834:["Fernando Cote"],
   835:["Lili"],
-  836:["Jesus Cisneros","Global / United States"],
+  836:["Jesus Cisneros","United States / Global"],
   837:["Anabel"],
   838:["Vitaly Kozyrev"],
   839:["Dimitrii Frolov"],
@@ -1208,4 +1208,4 @@ const facilitators = {
   1402:["Jessie K"],
   1403:["Steve Saunders"]
 };
-const cachedAt = "2025-08-30T08:03:04Z";
+const cachedAt = "2025-08-30T09:03:03Z";
