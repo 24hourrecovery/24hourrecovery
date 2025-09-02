38617a3f785cd98b14e100761c8974f2ddad91b5
24hourrecovery <smartrecoveryian@gmail.com>
2025-09-02 02:05:30 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -592,7 +592,7 @@ const facilitators = {
   77:["Pascale Hough"],
   78:["Valerie"],
   79:["Josee-Anne"],
-  80:["Jessica","Virginia / Ontario"],
+  80:["Jessica","Ontario / Virginia"],
   81:["Christine","Ontario"],
   82:["Ben","Ontario"],
   83:["Jean Clark"],
@@ -1208,4 +1208,4 @@ const facilitators = {
   1408:["Morgan"],
   1409:["Jacquelyn","Maryland"]
 };
-const cachedAt = "2025-09-02T06:03:04Z";
+const cachedAt = "2025-09-02T07:03:03Z";
