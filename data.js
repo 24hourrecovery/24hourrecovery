3525b49a68e1b88372f87957374a30f462332b2e
24hourrecovery <smartrecoveryian@gmail.com>
2026-02-18 02:19:06 -0600

    Automated commit: update data

--- data.js
+++ data.js
@@ -495,7 +495,7 @@ const meetings = [
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[1577],id:8921,language:"English",location:"Peaks Island, Maine",program:"4-Point Recovery",schedule:{day:2,time:"23:15Z"},type:"Both"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[1616],id:8923,language:"English",location:"Great Barrington, Massachusetts",program:"4-Point Recovery",schedule:{day:4,time:"22:00Z"},type:"Both"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[1576],id:8924,language:"English",location:"Minneapolis, Minnesota",program:"4-Point Recovery",schedule:{day:5,time:"20:00Z"},type:"Both"},
-  {audience:["Adults Welcome"],dataSource:"na",duration:120,facilitator:[1626],id:8929,language:"English",location:"Woburn, Massachusetts",program:"4-Point Recovery",schedule:{day:3,time:"03:30Z",week:[3]},type:"Online"},
+  {audience:["Adults Welcome"],dataSource:"na",duration:120,facilitator:[1626],id:8929,language:"English",location:"Woburn, Massachusetts",program:"4-Point Recovery",schedule:{day:3,time:"03:15Z",week:[3]},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[1602],id:8934,language:"English",location:"Raleigh, North Carolina",program:"4-Point Recovery",schedule:{day:3,time:"20:30Z"},type:"Both"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[1144],id:8938,language:"English",location:"Owenton, Kentucky",program:"4-Point Recovery",schedule:{day:1,time:"15:00Z"},type:"Online"},
   {audience:["Adults Welcome"],dataSource:"na",duration:60,facilitator:[1601],id:8939,language:"English",location:"Hartford, Connecticut",program:"4-Point Recovery",schedule:{day:3,time:"20:00Z"},type:"Online"},
@@ -1147,6 +1147,8 @@ const facilitators = {
   1528:["Jess","Georgia"],
   1530:["Chris","Ontario"],
   1531:["Soo-young bhang"],
+  1532:["Roman Prishchenko"],
+  1533:["Olesya Nilova"],
   1535:["Cheryl"],
   1537:["Nik"],
   1538:["Laura Westbrook"],
@@ -1279,4 +1281,4 @@ const facilitators = {
   1740:["Josh Albus"],
   1741:["Stefanie B"]
 };
-const cachedAt = "2026-02-18T05:17:04Z";
+const cachedAt = "2026-02-18T08:17:04Z";
