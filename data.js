4e20e42ee90d71c55ab4ee56389f33d03bce87b4
24hourrecovery <smartrecoveryian@gmail.com>
2026-05-19 05:18:25 -0500

    Automated commit: update data

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ const meetings = [
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[1291],id:1071,language:"English",location:"Frankston, Victoria",program:"Standard",schedule:{day:2,time:"05:00Z"},type:"Both"},
   {audience:["Drug and Alcohol Focus","Youth only"],dataSource:"au",duration:90,facilitator:[1053],id:1107,language:"English",location:"National",program:"Youth",schedule:{day:3,time:"09:00Z"},type:"Online"},
   {audience:["LGBTIQ+ only"],dataSource:"au",duration:90,facilitator:[29,1557],id:1126,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"08:00Z"},type:"Online"},
-  {audience:["All Welcome"],canceled:"2026-05-19 03:30:00-05",dataSource:"au",duration:90,facilitator:[1307,1796],id:1129,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"08:30Z"},type:"Online"},
+  {audience:["All Welcome"],canceled:"2026-05-26 03:30:00-05",dataSource:"au",duration:90,facilitator:[1307,1796],id:1129,language:"English",location:"National",program:"Standard",schedule:{day:2,time:"08:30Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[32],id:1182,language:"English",location:"National",program:"Standard",schedule:{day:1,time:"04:30Z"},type:"Online"},
   {audience:["All Welcome"],dataSource:"au",duration:90,facilitator:[33],id:1192,language:"English",location:"National",program:"Standard",schedule:{day:3,time:"04:30Z"},type:"Online"},
   {audience:["LGBTIQ+ only"],dataSource:"au",duration:90,facilitator:[29],id:1216,language:"English",location:"National",program:"Standard",schedule:{day:5,time:"08:00Z"},type:"Online"},
@@ -1441,4 +1441,4 @@ const facilitators = {
   1887:["Lorraine Sapet ACA Level 2 Registration: 24407"],
   1888:["Eliza"]
 };
-const cachedAt = "2026-05-19T07:17:04Z";
+const cachedAt = "2026-05-19T10:17:04Z";
